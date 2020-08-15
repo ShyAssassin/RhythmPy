706f0048f15947317162dca9f355df18a6d3a717
assassinsorrow <assassinsorrows@gmail.com>
2020-08-15 18:13:20 +0200

    added tests, main, requirements

---
+++ Mania Bot/Setup.py
@@ -0,0 +1,12 @@
+import os
+
+try:
+    os.system('pip install -r requirements.txt')
+    print('all dependencies have been installed!\n press ENTER to exit')
+    input()
+    exit()
+except:
+    print('something went wrong \n press ENTER to exit')
+    input()
+    exit()
+    
