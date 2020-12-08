c70c2b0bdb7e02739ba7336a373e6057cf4eb41a
assassinsorrow <assassinsorrows@gmail.com>
2020-12-08 23:37:56 +0200

    fixed main

--- RhythmPy/Main.py
+++ RhythmPy/Main.py
@@ -1,16 +1,7 @@
-import threading 
-import time 
-  
-def thread_foo(stop):
-    while True: 
-        print("THREAD STILL RUNNING!") 
-        if exit_flag: 
-            break
-exit_flag = False
-t = threading.Thread(target = thread_foo, args =(lambda : exit_flag, )) 
-t.start() 
-time.sleep(0.1) 
-print('Done sleeping! Time to stop the threads.')
-exit_flag = True
-t.join() 
-print('THREAD TERMINATED!')
+try:
+    from src import Run
+except ImportError:
+    from .src import Run
+
+if __name__ == "__main__":
+    Run()
