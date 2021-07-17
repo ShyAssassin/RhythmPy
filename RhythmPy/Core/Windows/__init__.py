9ad55a7c5132fdcc9058eb059fd2dd3876995b70
assassinsorrow <49711232+assassinsorrow@users.noreply.github.com>
2021-07-17 14:59:37 +0200

    Update __init__.py

--- RhythmPy/Core/Windows/__init__.py
+++ RhythmPy/Core/Windows/__init__.py
@@ -4,5 +4,6 @@ Platform = platform.system()
 if Platform == "Windows":
     # from .ApplySkins import ApplySkins
     from .WindowCapture import WindowCapture
+    from .DirectInput import keyDown, keyUp, press, typewrite
 else:
     pass
